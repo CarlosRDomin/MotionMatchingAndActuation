a0c5f1009c1abecb7b82e18332288d87d6dae498
Carlos Ruiz <carlos.ruiz.dominguez@west.cmu.edu>
2017-09-22 02:34:59 -0700

    Expanded functionality on generateDronesInRoomVideo
    Added frame rate and a saving video flag to further customize the
    behavior of generateDronesInRoomVideo

--- Helper functions/Plotting/generateDronesInRoomVideo.m
+++ Helper functions/Plotting/generateDronesInRoomVideo.m
@@ -1,28 +1,35 @@
-function generateDronesInRoomVideo(videoName, posUAVgt, roomDimensions, spotterCam)
+function generateDronesInRoomVideo(videoName, posUAVgt, roomDimensions, spotterCam, saveVideo, fps)
 	if isempty(videoName), videoName = 'videoSim.mp4'; end
+	if nargin<5 || isempty(saveVideo), saveVideo = true; end
+	if nargin<6 || isempty(fps), fps = 30; end
 	tempFrameName = 'frame.jpg';
 
 	% Initialize the video object
-	v = VideoWriter(videoName,'MPEG-4');
-	open(v);
+	if saveVideo
+		v = VideoWriter(videoName,'MPEG-4');
+		v.FrameRate = fps;
+		open(v);
+	end
 	
 	% For the first frame, need to create the figure and the handler
 	figure('Units','normalized', 'Position',[0.3 0.4 0.4 0.25]);
 	h=plotDronesInRoom(posUAVgt(:,1,:), roomDimensions, spotterCam);
-	saveas(gcf, tempFrameName);
-	writeVideo(v,imread(tempFrameName));
 	
 	% For all other frames, simply update XData, YData and ZData
-	for k=2:5:size(posUAVgt,2)
-		dispImproved(sprintf('\nProcessing video frame %4d out of %4d (%6.2f%%)', k, size(posUAVgt,2), 100*k/size(posUAVgt,2)));
-		%pause(1/(2*pointsPerIter)); % Play at 2x "real-time" 
+	for k=1:size(posUAVgt,2)
+		dispImproved(sprintf('Processing video frame %4d out of %4d (%6.2f%%)', k, size(posUAVgt,2), 100*k/size(posUAVgt,2)));
+		pause(1/fps);
 		set(h, 'XData',posUAVgt(:,k,1), 'YData',posUAVgt(:,k,2), 'ZData',posUAVgt(:,k,3));
-		saveas(gcf, tempFrameName);
-		writeVideo(v,imread(tempFrameName));
+		if saveVideo
+			saveas(gcf, tempFrameName);
+			writeVideo(v,imread(tempFrameName));
+		end
 	end
 	
 	% Done! :)
-	close(v);
-	delete(tempFrameName);
-	dispImproved(sprintf('\nDone saving simulation video!\n'), 'keepthis');
+	if saveVideo
+		delete(tempFrameName);
+		close(v);
+	end
+	dispImproved(sprintf('Done saving simulation video!\n'), 'keepthis');
 end
